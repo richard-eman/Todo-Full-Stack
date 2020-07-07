fcb9016d5752df26d13530733b8019c83c523819
Richard E <46254658+richard-eman@users.noreply.github.com>
2020-07-08 11:09:38 +1200

    initial commit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 class App extends React.Component {
+
   componentDidMount() {
+    console.log('App mount')
   }
 
   render() {
     return (
+      <>
       <h1>Todo</h1>
+      <h1>Nice</h1>
+      </>
     )
   }
 }
