2db4850f5194be8a06124b68e31a440ca3969a65
Richard E <46254658+richard-eman@users.noreply.github.com>
2020-07-08 13:32:46 +1200

    test db function

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,17 @@ const router = express.Router()
 
 const db = require('../db')
 
+// test route
 router.get('/yo', (req, res) => {
     res.sendStatus(200)
+})
+
+// test db function
+router.get('/hi', (req, res) => {
+  db.getTasks()
+    .then(tasks => {
+        res.json(tasks)
+    })
 })
 
 module.exports = router
