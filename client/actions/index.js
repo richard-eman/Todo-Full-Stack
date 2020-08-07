cdecaf3f994f83d400344ca9d83b434343e2723d
richard-eman <richardemanuel1205@gmail.com>
2020-08-07 13:04:22 +1200

    make edit task action

--- client/actions/index.js
+++ client/actions/index.js
@@ -3,6 +3,7 @@ import { getTasks } from "../apis"
 export const SHOW_ERROR = 'SHOW_ERROR'
 export const LIST_TASKS = 'LIST_TASKS'
 export const DELETE_TASK = 'DELETE_TASK'
+export const EDIT_TASK = 'EDIT_TASK'
 
 export function setListOfTasks(tasks) {
   return {
@@ -17,5 +18,12 @@ export function fetchTasks() {
     .then(tasks => {
       dispatch(setListOfTasks(tasks))
     })
+  }
+}
+
+export function setTaskToEdit(withObject) {
+  return {
+    type: EDIT_TASK,
+    task: withObject
   }
 }
