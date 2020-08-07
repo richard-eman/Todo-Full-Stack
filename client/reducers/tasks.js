d71a9291c1200e33d7545b3693a40119457d2238
richard-eman <richardemanuel1205@gmail.com>
2020-08-07 14:04:32 +1200

    set reducer for multiple actions

--- client/reducers/tasks.js
+++ client/reducers/tasks.js
@@ -1,11 +1,22 @@
-import { LIST_TASKS } from "../actions"
+import { LIST_TASKS, EDIT_TASK } from "../actions"
 
-const initialState = null
+const initialState = {
+  tasks: [],
+  task: {},
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LIST_TASKS:
-      return action.tasks
+      return {
+        ...state,
+        tasks: action.tasks,
+      }
+    case EDIT_TASK:
+      return {
+        ...state,
+        task: action.task,
+      }
     default:
       return state
   }
