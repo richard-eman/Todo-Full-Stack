aa8494097acbad8504c0549585a4899fd87dea89
richard-eman <richardemanuel1205@gmail.com>
2020-08-07 14:39:12 +1200

    make db edit function

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,19 @@ const config = require('../../knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
-function getTasks(db = connection){
-    return db('todos').select()
+function getTasks(db = connection) {
+  return db('todos').select()
+}
+
+function editTask(db = connection, task) {
+  return db('todos').where('id', task.id)
+  .update(
+    'task_name', task.name,
+    'details', task.details,
+    )
 }
 
 module.exports = {
-    getTasks,
+  getTasks,
+  editTask
 }
