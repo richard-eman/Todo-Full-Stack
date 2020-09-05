740c1497a8063e3e454ef315cb1bf327200d69db
richard-eman <richardemanuel1205@gmail.com>
2020-09-05 20:31:28 +1200

    add new form stuff to AddTask.jsx

--- client/components/AddTask.jsx
+++ client/components/AddTask.jsx
@@ -7,8 +7,8 @@ class AddTask extends React.Component {
   state = {
     name: "",
     details: "",
-    priority: 'ultra',
-    completed: 0,
+    priority: "",
+    completed: "0",
   }
 
   handleChange = e => {
@@ -20,6 +20,21 @@ class AddTask extends React.Component {
     else if ( e.target.name == "name"){
       this.setState({
         name: e.target.value,
+      })
+    }
+    else if ( e.target.name == "priority"){
+      this.setState({
+        priority: e.target.value,
+      })
+    }
+    else if ( e.target.name == "completed" && this.state.completed == "0"){
+      this.setState({
+        completed: "1",
+      })
+    }
+    else if ( e.target.name == "completed" && this.state.completed == "1"){
+      this.setState({
+        completed: "0",
       })
     }
   }
@@ -39,20 +54,33 @@ class AddTask extends React.Component {
         <h2>AddTask</h2>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
+            <input type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           <label htmlFor="details">Details</label>
-          <textarea
-            name="details"
-            value={this.state.details}
-            onChange={this.handleChange}
-          />
-          {/* <label htmlFor="priority">Priority</label> */}
-          {/* radio buttons go here */}
-          {/* also completion status */}
+            <textarea
+              name="details"
+              value={this.state.details}
+              onChange={this.handleChange}
+            />
+          <label htmlFor="priority">Priority</label>
+            <input type="radio"
+              name="priority"
+              value="Negative"
+              onChange={this.handleChange}
+            />
+            <input type="radio"
+              name="priority"
+              value="Ultra"
+              onChange={this.handleChange}
+            />
+          <label htmlFor="completed">Completed</label>
+            <input type="checkbox"
+              name="completed"
+              onChange={this.handleChange}
+            />
           <input type="submit" value="Save" />
         </form>
       </>
